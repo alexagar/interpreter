8ddac1b2b931bcf2ab645f30594e7ccc67088288
Alexa <agarc116@students.kennesaw.edu>
2022-03-06 16:51:02 -0500

    added get num of lines and ID table

--- Scanner.java
+++ Scanner.java
@@ -1,10 +1,34 @@
+/*
+ * Class:       CS 4308 Section 2
+ * Term:        Spring 2022
+ * Name:        Alexa Garcia
+ * Instructor:   Sharon Perry
+ * Project:     Deliverable P1 Scanner
+ */
+
+
 import java.io.*;
+import java.nio.file.*;
 
 public class Scanner{
     public static String[] tokens = {"function", "if", "else", "while", "for", "print",
-    "=","<=","<",">=",">","==","!=","+","-","*","/","%","\\","^", "end"};
+    "=","<=","<",">=",">","==","~=","+","-","*","/", "end"};
+
+    public static String[] tokensID = {"FUNC_KEY", "IF_KEY", "ELSE_KEY", "WHILE_KEY", "FOR_KEY", "PRINT_KEY",
+    "ASSIGN_OP", "LE_OP", "LT_OP", "GE_OP", "GT_OP", "EQ_OP", "NE_OP", "ADD_OP", "SUB_OP", "MUL_OP", "DIV_OP", "END_KEY"};
 
     public static void main(String[] args) throws Exception{
+        
+
+        //get number of lines
+        Path file = Paths.get("Test1.jl");
+        try{
+            long lines = Files.lines(file).count();
+        }catch(Exception e){
+            e.getStackTrace();
+        }
+
+        //begin reading in lines
         File f = new File("Test1.jl");
         BufferedReader br = new BufferedReader(new FileReader(f));
 
@@ -13,7 +37,7 @@ public class Scanner{
         int count = 1;
 
         while((s = br.readLine()) != null){
-            System.out.println("Line: " + count);
+            System.out.println("Line: " + count + " ");
             String str = s;
             if(isSubstring("function", str)){
                 funcStart++;
@@ -21,26 +45,30 @@ public class Scanner{
             if(funcStart > 0){
                 String[] splitStr = str.trim().split("\\s+");   
                 for(int i = 0; i < splitStr.length; i++){
-                    System.out.println(splitStr[i]);
                     System.out.println(lookup(splitStr[i]));
                 }
             }
             count++;
+            System.out.println();
         }
 
         
         br.close();
     }
 
-    public static int lookup(String name){
+    public static String lookup(String name){
         for(int i = 0; i < tokens.length; i++){
             if(name.equals(tokens[i])){
-                return i;
+                return (i + 5000) + " " + tokensID[i];
             }else if(isNumeric(name)){
-                return 89;
+                return 5018 + " LITERAL_INT";
+            }else if(isSubstring("print", name)){
+                return 5005 + " " + tokensID[5];
+            }else if(name.matches("[A-Za-z]{1}")){
+                return 5019 + " ID";
             }
         }
-        return -1;
+        return 5019 + " ID";
     }
 
     public static boolean isNumeric(String strNum) {
